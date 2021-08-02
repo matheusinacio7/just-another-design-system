cd1ab68e087b0236631d722c2dc6fe8e08c2ffc9
heyset <c.inaciomatheus@gmail.com>
2021-08-02 00:43:58 -0300

    Sera q agora vai

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 #! /usr/bin/env node
 
 const fs = require('fs');
+const path = require('path');
+
+const getModuleSource = (moduleName) => path.join(__dirname, moduleName);
+const getModuleDestination = (moduleName) => path.join(process.cwd(), moduleName);
+
+fs.promises.copyFile(getModuleSource('globals.css'), getModuleDestination('globals.css'), fs.constants.COPYFILE_EXCL, (err) => {
+  if (err) throw err;
+  console.log('deu bom');
+})
